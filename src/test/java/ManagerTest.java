a30af92d0a0b31fbfbb609ac8ed2c770422b953c
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-19 19:57:52 +0100

    new manager made in test

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,15 @@
+import models.Manager;
+import org.junit.Before;
+
+public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Lionel Spencer-Clark", "01847 893245", "lionel.lad@btinternet.com");
+    }
+
+    public
+
+}
