95a94d49898c1fd7f3f41c66d23f547452fc722c
leeburgess67 <leeburgessmail@gmail.com>
2018-07-19 20:41:29 +0100

    Fixed missing } in manager test

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -16,7 +16,7 @@ public class ManagerTest {
     public void before(){
         League league = new League("North of England Regional Division", LeagueType.NATIONAL, "Lancashire");
         manager = new Manager("Lionel Spencer", "01847 873245", "lionel@btinternet.com");
-        team = new FootballTeam("Bury", manager, league, "d", "Bury Saint Edmonds");
+        team = new FootballTeam("Bury", manager, league, "d", "Bury Saint Edmonds");}
 
     public void setUp(){
         manager = new Manager("John", "0760456","john.gmail.com");
