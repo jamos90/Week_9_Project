9378107d4b95cd0270a9d3c7a4a1e498dc5c85c6
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-19 20:51:11 +0100

    fixture updates

--- src/test/java/FixtureTest.java
+++ src/test/java/FixtureTest.java
@@ -8,7 +8,8 @@ import static junit.framework.Assert.assertEquals;
 public class FixtureTest {
 
     Fixture fixture;
-    FootballTeam footballTeam;
+    FootballTeam homefootballTeam;
+    FootballTeam awayfootballTeam;
     Manager manager;
     League league;
     MatchReport matchReport1;
@@ -18,7 +19,8 @@ public class FixtureTest {
     public void before() {
         manager = new Manager("Zsolt Poboda-Salai", "07610335467", "Zsolt@hungarianmail.hu");
         league = new League("North of England Regional Division", LeagueType.NATIONAL, "Lancashire");
-        footballTeam = new FootballTeam("Bolton Wanderers", manager, league, "nothing", "Bolton");
+        homefootballTeam = new FootballTeam("Bolton Wanderers", manager, league, "nothing", "Bolton");
+        awayfootballTeam = new FootballTeam("Dagenham and Redbridge", manager, league, "yes", "Dagenham");
         fixture = new Fixture(1, "Liberty Stadium", league);
         matchReport1 = new MatchReport(fixture, "Wanderers slaughtered in humiliating rout!", "Same", "logo");
     }
@@ -71,8 +73,10 @@ public class FixtureTest {
     }
 
     @Test
-    public void canSetHomeTeam(){
-
+    public void canSetTeams(){
+        fixture.addTeamsToFixture(homefootballTeam, awayfootballTeam);
+        assertEquals(awayfootballTeam, fixture.getAwayTeam());
+        assertEquals(homefootballTeam, fixture.getHomeTeam());
     }
 
 }
