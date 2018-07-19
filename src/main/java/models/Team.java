b10313c97aaa517e9801c895a1406c49bc20bde4
jamos90 <jamos_98@hotmail.com>
2018-07-19 13:22:24 +0100

    team abstract class set up. Getters and setters created

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,77 @@
 package models;
 
-public class Team {
+public abstract class Team {
+    private int id;
+    private String name;
+    private Manager manager;
+    private League league;
+    private int points;
+    private String teamLogo;
+    private String homePitch;
+
+    public Team(){}
+
+    public Team(String name, Manager manager, League league, int points, String teamLogo){
+        this.name = name;
+        this.manager = manager;
+        this.league = league;
+        this.points = points;
+        this.teamLogo = teamLogo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public League getLeague() {
+        return league;
+    }
+
+    public void setLeague(League league) {
+        this.league = league;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getTeamLogo() {
+        return teamLogo;
+    }
+
+    public void setTeamLogo(String teamLogo) {
+        this.teamLogo = teamLogo;
+    }
+
+    public String getHomePitch() {
+        return homePitch;
+    }
+
+    public void setHomePitch(String homePitch) {
+        this.homePitch = homePitch;
+    }
 }
