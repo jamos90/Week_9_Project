72e3c2159e7a9b3f76e12aca0614357540b185f0
jamos90 <jamos_98@hotmail.com>
2018-07-19 13:27:43 +0100

    football team  and get/setters created

--- src/main/java/models/FootballTeam.java
+++ src/main/java/models/FootballTeam.java
@@ -1,4 +1,28 @@
 package models;
 
-public class FootballTeam {
+public class FootballTeam extends Team {
+    private int goalsScored;
+    private int goalsConceded;
+
+    public FootballTeam(String name, Manager manager, League league, int points, String teamLogo, int goalsScored, int goalsConceded){
+        super(name, manager,league,points,teamLogo);
+        this.goalsScored = goalsScored;
+        this.goalsConceded =goalsConceded;
+    }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+
+    public int getGoalsConceded() {
+        return goalsConceded;
+    }
+
+    public void setGoalsConceded(int goalsConceded) {
+        this.goalsConceded = goalsConceded;
+    }
 }
