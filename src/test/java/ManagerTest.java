1e6fada6fc2c7f6bd27d0193b280e2f7148a9dd3
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-19 20:06:28 +0100

    all manager tests fully functional

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,4 +1,4 @@
-import models.Manager;
+import models.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -6,11 +6,15 @@ import static junit.framework.Assert.assertEquals;
 
 public class ManagerTest {
 
+    FootballTeam team;
     Manager manager;
+    League league;
 
     @Before
     public void before(){
+        League league = new League("North of England Regional Division", LeagueType.NATIONAL, "Lancashire");
         manager = new Manager("Lionel Spencer-Clark", "01847 873245", "lionel.lad@btinternet.com");
+        team = new FootballTeam("Bury", manager, league, "d", "Bury Saint Edmonds");
     }
 
     @Test
@@ -26,6 +30,24 @@ public class ManagerTest {
     @Test
     public void hasEmailAddress(){
         assertEquals("lionel.lad@btinternet.com", manager.getEmail());
+    }
+
+    @Test
+    public void canSetTeam(){
+        manager.setTeam(team);
+        assertEquals(team, manager.getTeam());
+    }
+
+    @Test
+    public void canUpdatePhoneNumber(){
+        manager.setPhoneNo("01847 873244");
+        assertEquals("01847 873244", manager.getPhoneNo());
+    }
+
+    @Test
+    public void canUpdateEmailAddress(){
+        manager.setEmail("lionel.spencer.clark@aol.com");
+        assertEquals("lionel.spencer.clark@aol.com", manager.getEmail());
     }
 
 }
