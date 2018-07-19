04b7b823ad5b59d2d2542649972805120cde6d40
leeburgess67 <leeburgessmail@gmail.com>
2018-07-19 14:14:29 +0100

    fixture class and getters/ setters created

--- src/main/java/models/Fixture.java
+++ src/main/java/models/Fixture.java
@@ -1,4 +1,67 @@
 package models;
 
 public class Fixture {
+    private Team homeTeam;
+    private Team awayTeam;
+    private int homeGoals;
+    private int awayGoals;
+    private int week;
+    private String venue;
+
+    public Fixture(){}
+
+    public Fixture(Team homeTeam, Team awayTeam, int homeGoals, int awayGoals, int week, String venue){
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.week = week;
+        this.venue = venue;
+    }
+
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(Team homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(Team awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public int getHomeGoals() {
+        return homeGoals;
+    }
+
+    public void setHomeGoals(int homeGoals) {
+        this.homeGoals = homeGoals;
+    }
+
+    public int getAwayGoals() {
+        return awayGoals;
+    }
+
+    public void setAwayGoals(int awayGoals) {
+        this.awayGoals = awayGoals;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
 }
--- src/main/java/models/FootballTeam.java
+++ src/main/java/models/FootballTeam.java
@@ -4,11 +4,14 @@ public class FootballTeam extends Team {
     private int goalsScored;
     private int goalsConceded;
 
+
     public FootballTeam(String name, Manager manager, League league, int points, String teamLogo, int goalsScored, int goalsConceded, String location){
         super(name, manager,league,points,teamLogo,location);
         this.goalsScored = goalsScored;
         this.goalsConceded = goalsConceded;
     }
+
+    public FootballTeam(){};
 
     public int getGoalsScored() {
         return goalsScored;
