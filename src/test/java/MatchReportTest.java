02be34177db934e52ca390100d740d861d9401df
jamos90 <jamos_98@hotmail.com>
2018-07-19 20:26:01 +0100

    match report: has fixture test passed

--- src/test/java/MatchReportTest.java
+++ src/test/java/MatchReportTest.java
@@ -1,1 +1,27 @@
+import models.Fixture;
+import models.League;
+import models.LeagueType;
+import models.MatchReport;
+import org.junit.Before;
+import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+
+public class MatchReportTest{
+
+    MatchReport matchReport;
+    Fixture fixture;
+    League league;
+
+    @Before
+    public void setUp(){
+        fixture = new Fixture(1,"Edinburgh",league);
+        league = new League("Edinburgh Schools", LeagueType.SCHOOLS, "Midlothian");
+        matchReport = new MatchReport(fixture,"one", "two", "pic.jpg");
+    }
+    @Test
+    public void hasFixture(){
+        assertEquals(fixture, matchReport.getFixture());
+    }
+
+}
