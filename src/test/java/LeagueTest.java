936825c384b3f13966569b9e59e16336e90f5120
jamos90 <jamos_98@hotmail.com>
2018-07-19 19:04:54 +0100

    league test created and has name test passed

---
+++ src/test/java/LeagueTest.java
@@ -0,0 +1,27 @@
+
+import models.Fixture;
+import models.League;
+import models.LeagueType;
+import models.Team;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class LeagueTest {
+    League league;
+    Fixture fixture;
+    Team team;
+    LeagueType leagueType;
+
+    @Before
+    public void setUp(){
+        league = new League("Edinburgh Schools", LeagueType.SCHOOLS, "Midlothian");
+        fixture = new Fixture(1,"Edinburgh Football Club", league);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Edinburgh Schools", league.getName());
+    }
+}
