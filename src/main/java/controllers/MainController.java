f883777d7f2d63ff2e3884290a327282acb37446
jamos90 <jamos_98@hotmail.com>
2018-07-21 12:58:09 +0100

    main view and managers view working

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -2,13 +2,32 @@ package controllers;
 
 import db.Seeds;
 import models.FootballTeam;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.ObjLongConsumer;
+
+import static spark.Spark.get;
 
 public class MainController {
     public static void main(String[] args) {
 
 
+        Seeds.seedData();
         ManagerController managerController = new ManagerController();
-        FootballTeamController footballTeamController = new FootballTeamController();
+//        FootballTeamController footballTeamController = new FootballTeamController();
+
+        get("/all", (req,res)-> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/index.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+    }, new VelocityTemplateEngine());
+
+
 
     }
 }
--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -1,9 +1,83 @@
 package db;
 
+import models.*;
+
 public class Seeds {
 
     public static void seedData(){
 
+
+        DBHelper.deleteAll(Manager.class);
+        DBHelper.deleteAll(Team.class);
+        DBHelper.deleteAll(MatchReport.class);
+        DBHelper.deleteAll(FootballTeam.class);
+        DBHelper.deleteAll(League.class);
+        DBHelper.deleteAll(Fixture.class);
+
+        Manager manager = new Manager("Zsolt Poboda-Salai", "07610335467", "Zsolt@hungarianmail.hu");
+        DBHelper.save(manager);
+
+        League league = new League("North of England Regional Division", LeagueType.NATIONAL, "Lancashire");
+        DBHelper.save(league);
+
+        FootballTeam homefootballTeam = new FootballTeam("Bolton Wanderers", manager, league, "nothing", "Bolton");
+        DBHelper.save(homefootballTeam);
+
+        FootballTeam awayfootballTeam = new FootballTeam("Dagenham and Redbridge", manager, league, "yes", "Dagenham");
+        DBHelper.save(awayfootballTeam);
+
+        FootballTeam thirdFootballTeam = new FootballTeam("Burton Albion", manager, league, "yes", "Burton");
+        DBHelper.save(thirdFootballTeam);
+
+        FootballTeam fourthfootballTeam = new FootballTeam("Edinburgh City", manager, league, "no", "Edinburgh");
+        DBHelper.save(fourthfootballTeam);
+
+        FootballTeam fithfootballTeam = new FootballTeam("Glasgow City", manager, league, "no", "Edinburgh");
+        DBHelper.save(fithfootballTeam);
+
+        FootballTeam sixthfootballTeam = new FootballTeam("Dundee City", manager, league, "no", "Edinburgh");
+        DBHelper.save(sixthfootballTeam);
+
+        FootballTeam seventhfootballTeam = new FootballTeam("Aberdeen City", manager, league, "no", "Edinburgh");
+        DBHelper.save(seventhfootballTeam);
+
+        FootballTeam eighthfootballTeam = new FootballTeam("FortWilliam City", manager, league, "no", "Edinburgh");
+        DBHelper.save(eighthfootballTeam);
+
+        FootballTeam ninethfootballTeam = new FootballTeam("Inverness City", manager, league, "no", "Edinburgh");
+        DBHelper.save(ninethfootballTeam);
+
+        FootballTeam tenthfootballTeam = new FootballTeam("Random City", manager, league, "no", "Edinburgh");
+        DBHelper.save(tenthfootballTeam);
+
+        league.addToTeams(awayfootballTeam);
+        league.addToTeams(homefootballTeam);
+        league.addToTeams(thirdFootballTeam);
+        league.addToTeams(fourthfootballTeam);
+        league.addToTeams(fithfootballTeam);
+        league.addToTeams(sixthfootballTeam);
+        league.addToTeams(seventhfootballTeam);
+        league.addToTeams(eighthfootballTeam);
+        league.addToTeams(ninethfootballTeam);
+        league.addToTeams(tenthfootballTeam);
+
+
+        league.generateFixtures(true);
+
+
+
+        DBHelper.update(league);
+        for (Fixture fixture : league.getFixtures()) {
+            DBHelper.save(fixture);
+        }
+
+        Fixture f = new Fixture(4,league);
+        f.addAwayTeamToFixture(awayfootballTeam);
+        f.addHomeTeamToFixture(fourthfootballTeam);
+        DBHelper.save(f);
+
+        MatchReport report1 = new MatchReport(f, "Dagenham pull of shock victory at Edinburgh", "Against all expectations, Dagenham and Redbridge eked out a battling win away at Edinburgh, their first away points of the season.", "n");
+        DBHelper.save(report1);
 
     }
 }
