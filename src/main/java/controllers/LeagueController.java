b6369a369d7787e9bb35bdc511f8f87dbc3a5ba4
jamos90 <jamos_98@hotmail.com>
2018-07-22 10:56:52 +0100

    delete leagues working

--- src/main/java/controllers/LeagueController.java
+++ src/main/java/controllers/LeagueController.java
@@ -3,11 +3,17 @@ package controllers;
 import db.DBHelper;
 import models.Fixture;
 import models.League;
+import models.Team;
+import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class LeagueController {
     public LeagueController(){
@@ -22,11 +28,22 @@ public class LeagueController {
             model.put("leagues", leagues);
             List<Fixture> fixtures = DBHelper.getAll(Fixture.class);
             model.put("fixtures", fixtures);
+            List<Team> teams = DBHelper.getAll(Team.class);
+            model.put("teams", teams);
+            model.put("template", "templates/leagues/index.vtl");
+            return new ModelAndView(model,"templates/layout.vtl");
+        }, new VelocityTemplateEngine());
 
-
-
+        post("/leagues/:id/delete", (req,res)->{
+            int leagueToDeleteId = Integer.parseInt(req.params("id"));
+            League leagueToDelete = DBHelper.find(leagueToDeleteId, League.class);
+            DBHelper.delete(leagueToDelete);
+            res.redirect("/leagues");
+            return null;
         }, new VelocityTemplateEngine());
 
 
         }
+
+
 }
