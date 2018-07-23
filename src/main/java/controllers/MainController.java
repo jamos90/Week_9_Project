bfb900fc032c48daccac6c5b7df9959f34b8bd97
leeburgess67 <leeburgessmail@gmail.com>
2018-07-23 18:05:33 +0100

    css files linked

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -13,9 +13,12 @@ import java.util.Map;
 import java.util.function.ObjLongConsumer;
 
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 public class MainController {
     public static void main(String[] args) {
+
+        staticFileLocation("/public");
 
 
         Seeds.seedData();
